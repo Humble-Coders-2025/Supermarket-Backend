6339f121733f8c91b3adb1cf8f8c41c2b24ce099
Garvit Gulati <ggulati_be22@thapar.edu>
2025-03-17 19:42:30 +0530

    fix ports

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ sequelize
         console.log("Connected to database");
 
         // Start the server
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
+        app.listen(8080, () => {
+            console.log(`Server is running on port 8080`);
         });
     })
     .catch((err) => {
