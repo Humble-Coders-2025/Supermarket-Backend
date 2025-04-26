edc2978e662c957998cc10a634780a723960615d
Garvit Gulati <ggulati_be22@thapar.edu>
2025-04-26 17:43:17 +0530

    add admin router

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ const router = require("express").Router();
 const customerRouter = require("./customer/index.js");
 router.use("/customer", customerRouter);
 
+const adminRouter = require("./admin/index.js");
+router.use("/admin", adminRouter);
+
 module.exports = router;
